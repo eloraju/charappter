0b5e7eadb02682ec253e1751303d81171ee8574d
Juuso Elo-Rauta <juuso.er@gmail.com>
2021-02-21 22:09:23 +0200

    removed testfile

--- joi.ts
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-console.dir(joi.object({
-    test: joi.string().required(),
-    num: joi.number().min(10)
-}).validate({
-    test: "asdf",
-    num: 1
-}).error, {depth: null});
